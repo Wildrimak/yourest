40767e20574b9004d6be08336293c6c6d8a593a1
Wildrimak <finalwildrimak@gmail.com>
2020-08-30 02:11:09 -0300

    rt-18: Added ChannelInputModel and your requirements

---
+++ src/main/java/com/wildrimak/yourest/api/models/ChannelInputModel.java
@@ -0,0 +1,13 @@
+package com.wildrimak.yourest.api.models;
+
+import lombok.Data;
+
+@Data
+public class ChannelInputModel {
+
+	private String name;
+	private String description;
+	private String location;
+	private String art;
+	
+}
